f3426a758f549f96aadd1606e561da3f938c753c
yar-kik <einstein16.04@gmail.com>
2020-07-14 19:12:38 +0300

    update settings

--- konfetka/konfetka/settings.py
+++ konfetka/konfetka/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'first_test',
-    'second_test',
     'articles',
     'taggit',
     'django.contrib.sites',
