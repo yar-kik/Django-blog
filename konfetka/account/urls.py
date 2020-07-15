51f91011946e406a5091a04636ace768eaad2d3b
yar-kik <einstein16.04@gmail.com>
2020-07-16 01:32:37 +0300

    change password form

--- konfetka/account/urls.py
+++ konfetka/account/urls.py
@@ -6,6 +6,10 @@ app_name = 'account'
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='account/registration/logged_out.html'), name='logout'),
+    # Для уточнення місцезнаходження шаблона
+    # path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
