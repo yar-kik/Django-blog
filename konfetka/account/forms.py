25a9d7fa1c478cff222397db8b191a0a523158af
yar-kik <einstein16.04@gmail.com>
2020-07-16 23:06:01 +0300

    reset password form, registration form

--- konfetka/account/forms.py
+++ konfetka/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -22,3 +23,17 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords don't match!")
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    """Дозволяє користувачам змінювати ім'я, прізвише та пошту"""
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    """Дозволяє """
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
--- konfetka/konfetka/urls.py
+++ konfetka/konfetka/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from articles.sitemaps import ArticleSitemap
 
@@ -28,3 +30,6 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap')
 ]
+"""Don't use at working website!"""
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
