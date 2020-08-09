a53291005afd67f0ce5acfaa6afd17e7211574e3
yar-kik <einstein16.04@gmail.com>
2020-08-10 01:50:26 +0300

    delete article's function

--- konfetka/articles/urls.py
+++ konfetka/articles/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
              views.article_detail, name='article_detail'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/update_article/', views.UpdateArticle.as_view(),
          name='update_article'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/delete_article/', views.DeleteArticle.as_view(),
+         name='delete_article'),
     path('<int:article_id>/share/', views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', views.all_articles, name='all_articles_by_tag'),
     path('feed/', LatestArticlesFeed(), name='article_feed'),
