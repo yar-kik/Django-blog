79d31ac07881b65c8b01e9244498f96b27f4223b
yar-kik <einstein16.04@gmail.com>
2020-08-25 19:24:59 +0300

    a little bit change field photo

--- konfetka/account/models.py
+++ konfetka/account/models.py
@@ -11,8 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='user/%Y/%m/%d/', blank=True, default='default/profile-picture.png.')
-
+    photo = models.ImageField(upload_to='user/profile_photo', blank=True, default='default/profile-picture.png')
     # phone = PhoneField(blank=True, help_text='Enter phone number')
     # phone = PhoneNumberField()
 
