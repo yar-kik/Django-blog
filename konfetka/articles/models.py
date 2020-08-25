a9ab25e1a595df1720f0af170b1808fa89d9c897
yar-kik <einstein16.04@gmail.com>
2020-08-25 19:21:50 +0300

    order by date created

--- konfetka/articles/models.py
+++ konfetka/articles/models.py
@@ -34,7 +34,7 @@ class Comment(models.Model):
     active = models.BooleanField(default=True)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('-created',)
 
     def __str__(self):
         return f'Comment by {self.name} on {self.article}'
