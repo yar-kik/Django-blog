6fc3250b7f1c58f7b28c09ace1b8fe2c3a7fc750
yar-kik <einstein16.04@gmail.com>
2021-01-23 16:59:04 +0200

    bug hotfix 2

--- account/views.py
+++ account/views.py
@@ -45,18 +45,8 @@ def user_login(request):
 @login_required
 def dashboard(request):
     """За замовчуванням відображаємо всі дії"""
-    actions = Action.objects.exclude(user=request.user)
-    following_ids = request.user.following.values_list('id', flat=True)
     profile = request.user.profile
-    if following_ids:
-        """Якщо поточний користувач підписався на когось
-        відображаємо тільки дії циї користувачів"""
-        actions = actions.filter(user_id__in=following_ids)
-    actions = actions.select_related('user', 'user__profile').\
-        prefetch_related('target')[:10]
-
     return render(request, 'account/dashboard.html', {'section': 'dashboard',
-                                                      'actions': actions,
                                                       'profile': profile})
 
 
